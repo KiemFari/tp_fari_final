1e5ad5b050605ce9ccd8fee764bc3a998258b86a
Kiemtore_Fariratou <kiemtorefariratou@.com>
2023-11-08 13:24:13 +0000

    master

---
+++ index.php
@@ -0,0 +1,53 @@
+
+<!-- <?php
+  session_start();
+  session_destroy();
+  if( isset($_SESSION['id_user']) && !empty($_SESSION['id_user']) ){
+
+    header("location:connexion.php");
+    exit;
+
+}else{
+      
+ } 
+
+?> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+ integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+  crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+   rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
+    <title>Site E_CUMMERCE</title>
+</head>
+<body>
+           <?php include ("nav.php"); ?>
+       
+     <!-- <?php include ("fari.php"); ?>  -->
+<!-- <header> 
+         <nav>
+            <ul>
+                <li><a href="Acceuil.php">Acceuil</a></li>
+                <li><a href="Service.php">Service</a></li>
+                <li><a href="Galerie.php">Galerie culturelle</a></li>
+                <li><a href="Contact.php">Contacter</a></li>
+            </ul>
+            <a href="" class="btn-reservation">Visiter Maintenant</a>
+        </nav> 
+    
+    </header> -->
+<!-- ------acceuil section --------->
+
+
+
+<!-- lier footer -->
+<!-- <?php include ("footer.php")?> -->
+
+    </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,21 @@
+
+<header>
+
+    <div class="logo">
+<a href="index.php"><span>E_COMMERCE</span>"MA COMMANDE"</a>
+    </div>
+    
+<nav>
+            <ul class="menu">
+                <li><a href="index.php">Acceuil</a></li>
+                <li><a href="Achat.php">Achat</a></li>
+                <li><a href="Galeriess.php">SERVICES</a></li>
+                <li><a href="Blog.php">Blog</a></li>
+                <li><a href="Contact.php">Contact</a></li>
+                <li><a href="inscription.php">inscription</a></li>
+                <li><a href="connexion.php">connexion</a></li>
+                <li><a href="deconnection.php">deconnexion</a></li>
+            </ul>
+          
+        </nav>
+        </header> 
